4462ad45f39bce3b3e21bb94f0d855cc3dd83f2b
张翔 <572545988@qq.com>
2025-01-14 13:44:42 +0800

    feat: 🎸 add proxy number

--- index.ts
+++ index.ts
@@ -6,9 +6,10 @@ import loadXLSFile from './loadXLSFile.ts';
  * 检查 SOCKS5 代理 IP 是否有效。
  *
  * @param {string} proxyUrl - SOCKS5 代理地址，格式为 socks5h://user:password@ip:port。
+ * @param {number} index - 代理序号。
  * @returns {Promise<boolean>} - 如果代理有效返回 true，否则返回 false。
  */
-async function checkProxy(proxyUrl: string) {
+async function checkProxy(proxyUrl: string, index: number) {
   try {
     // 创建代理 agent
     const agent = new SocksProxyAgent(proxyUrl);
@@ -18,14 +19,14 @@ async function checkProxy(proxyUrl: string) {
     });
 
     if (!response.ok) {
-      console.error(`❌代理无效！代理: ${proxyUrl}`);
+      console.error(`❌代理无效！代理${index}: ${proxyUrl}`);
     }
 
     const data = await response.json();
-    console.log(`✅代理有效！代理: ${proxyUrl}，返回 IP: ${data.ip}`);
+    console.log(`✅代理有效！代理${index}: ${proxyUrl}，返回 IP: ${data.ip}`);
     return true;
   } catch (error) {
-    console.error(`❌代理无效！代理: ${proxyUrl}`);
+    console.error(`❌代理无效！代理${index}: ${proxyUrl}`);
     return false;
   }
 }
@@ -37,8 +38,8 @@ async function checkProxy(proxyUrl: string) {
  */
 async function checkProxies() {
   const proxies = await loadXLSFile('./config.xlsx');
-  for (const proxyUrl of proxies) {
-    await checkProxy(proxyUrl);
+  for (const [index, proxyUrl] of proxies.entries()) {
+    await checkProxy(proxyUrl, index+1);
   }
 }
 
